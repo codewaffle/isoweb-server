2f26596d470f9ade6eaf32cdc16a99da5f67b740
Patrick Griffus <patrick@codewaffle.com>
2015-07-05 17:23:32 -0400

    mathx.Rect

--- mathx.py
+++ mathx.py
@@ -1,5 +1,5 @@
 # this file should mirror mathx.coffee
-import math
+from math import floor, ceil, sqrt
 
 
 class RNG(object):
@@ -41,11 +41,77 @@ class Vector2(object):
 
     @property
     def magnitude(self):
-        return math.sqrt(self.dot(self))
+        return sqrt(self.dot(self))
 
     @property
     def sqr_magnitude(self):
         return self.dot(self)
 
     def __repr__(self):
-        return 'Vec2({s.x}, {s.y})'.format(s=self)
+        return 'Vector2({s.x}, {s.y})'.format(s=self)
+
+class Rect(object):
+    __slots__ = ['top', 'right', 'bottom', 'left']
+
+    def __repr__(self):
+        return 'Rect({s.left}, {s.bottom}, {s.right}, {s.top})'.format(s=self)
+
+    def __init__(self, left=0, bottom=0, right=0, top=0):
+        self.left = int(min(left, right))
+        self.right = int(max(left, right))
+        self.top = int(max(bottom, top))
+        self.bottom = int(min(bottom, top))
+
+    def __contains__(self, item):
+        if isinstance(item, Vector2):
+            return self.left <= item.x < self.right and self.bottom <= item.y < self.top
+        elif isinstance(item, Rect):
+            return item.left >= self.left and item.right <= self.right and item.top <= self.top and item.bottom >= self.bottom
+
+    def set_bounds(self, left, bottom, right, top):
+        self.left = int(min(left, right))
+        self.right = int(max(left, right))
+        self.top = int(max(bottom, top))
+        self.bottom = int(min(bottom, top))
+        return self
+
+    def set_center(self, center):
+        diff = self.center - center
+        self.left = int(self.left + diff.x)
+        self.right = int(self.right + diff.x)
+        self.top = int(self.top + diff.y)
+        self.bottom = int(self.bottom + diff.y)
+        return self
+
+    def set_width(self, val):
+        diff = (val - self.width) / 2.
+        self.left = int(self.left - ceil(diff))
+        self.right = int(self.right + floor(diff))
+        return self
+
+    def set_height(self, val):
+        diff = (val - self.height) / 2.
+        self.top = int(self.top + ceil(diff))
+        self.bottom = int(self.bottom - floor(diff))
+        return self
+
+    def set_size(self, width, height):
+        return self.set_width(width).set_height(height)
+
+    def expand(self, left, bottom, right, top):
+        return self.set_bounds(min(left, self.left), min(bottom, self.bottom), max(right, self.right), max(top, self.top))
+
+    def contract(self, left, bottom, right, top):
+        return self.set_bounds(max(left, self.left), max(bottom, self.bottom), min(right, self.right), min(top, self.top))
+
+    @property
+    def width(self):
+        return self.right - self.left
+
+    @property
+    def height(self):
+        return self.top - self.bottom
+
+    @property
+    def center(self):
+        return Vector2((self.right - self.left) / 2., (self.top - self.bottom) / 2.)
