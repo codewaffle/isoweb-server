8915d9d60bf2d8a2ff6b3e7a338883a58c79496a
Patrick Griffus <patrick@codewaffle.com>
2015-07-12 01:49:19 -0400

    simple groundwork for entity references.

--- entity.py
+++ entity.py
@@ -1,6 +1,16 @@
 from collections import defaultdict
 from util import memoize, AttributeDict
 
+class EntityReference(object):
+    """An invalidate-able reference to an Entity.
+
+    When the Entity is disposed, .valid will be set false and the Entity can be
+    returned to the pool after its memoize has been cleared.. and it will generate a new reference on demand.
+    """
+    def __init__(self, entity):
+        self.entity = entity
+        self.valid = True
+
 
 class Entity(object):
     def __init__(self, entity_def):
@@ -13,3 +23,6 @@ class Entity(object):
         # return memoized component proxy
         return getattr(self.entity_def, item).proxy(self)
 
+    @memoize
+    def reference(self):
+        return EntityReference(self)
