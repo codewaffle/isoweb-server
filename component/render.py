d84f30ab91cf863cfe25100f1a7a042b22bfd4c9
Patrick Griffus <patrick@codewaffle.com>
2015-07-19 23:34:18 -0400

    drop MESH_UPDATE in favor of simplicity. We'll probably be doing some gzip on the whole stream at some point anyway..

--- component/render.py
+++ component/render.py
@@ -2,8 +2,6 @@ from functools import partial
 from time import time
 from component import BaseComponent
 from component.base import string_replicator
-import packet_types
-
 
 class Mesh(BaseComponent):
     data = {
@@ -16,23 +14,8 @@ class Mesh(BaseComponent):
 
     @classmethod
     def initialize(cls, entity, data):
-        entity.snapshots[entity.Mesh.mesh_update] = time()
-
-    @classmethod
-    def mesh_update(cls, entity, data):
-        if data.material['type'] == 'MeshLambertMaterial':
-            mat_type = 1
-        else:
-            mat_type = 124
-
-        model_len = len(data.model)
-        map_len = len(data.material['map'])
-
-        return 'HBB{}sB{}s'.format(
-            model_len, map_len
-        ), [
-            packet_types.MESH_UPDATE, mat_type, model_len, data.model, map_len, data.material['map']
-        ]
+        entity.snapshots[string_replicator(partial(getattr, data, 'model'), 'model')] = time()
+        entity.snapshots[string_replicator(partial(data.material.get, 'map'), 'map')] = time()
 
 class Sprite(BaseComponent):
     data = {
