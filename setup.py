525ae036d43141f68328bf004d16ca8d6f524326
Patrick Griffus <patrick@codewaffle.com>
2015-10-18 17:22:22 -0400

    chipmunk is back on the menu

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from distutils import msvccompiler
+
 try:
     from setuptools import setup
     from setuptools import Extension
@@ -7,12 +9,43 @@ except ImportError:
 
 from Cython.Build import cythonize
 
+import os
+import glob
 
+define_macros = []
+extra_compile_args = []
+
+if os.name == 'nt':
+    msvccompiler.MSVCCompiler._c_extensions = []
+    msvccompiler.MSVCCompiler._cpp_extensions.append('.c')
+
+    define_macros = [
+        # stupid build hacks..
+        ('WIN32', '1'), ('inline', '__inline')
+    ]
+
+    extra_compile_args=[
+        # more stupid build hacks
+        '/TP',
+    ]
+
+chipmunk_src = glob.glob('phys/src/*.c')
 
 extensions = [
+    Extension(
+        'phys.chipmunk',
+        chipmunk_src + ['phys/chipmunk.pyx'],
+        include_dirs=['phys/include'],
+        define_macros=define_macros,
+        extra_compile_args=extra_compile_args,
+    ),
+    Extension(
+        'phys.*', ['phys/**/*.pyx'],
+    ),
     Extension('*', ['**/*.pyx']),
 ]
 
 setup(
     ext_modules=cythonize(extensions),
 )
+
